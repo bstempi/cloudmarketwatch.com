3a3afdd5df39a8de433b016e5728823d8e4f3edc
Brian Stempin <brian.stempin@gmail.com>
2013-12-17 02:55:39 -0500

    Broke apart some of the code into other methods and added TODOs for later

--- BackendBundle/Command/AwsUpdateCommand.php
+++ BackendBundle/Command/AwsUpdateCommand.php
@@ -12,6 +12,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+// TODO Add proper logging
 class AwsUpdateCommand extends ContainerAwareCommand {
 	protected function configure() {
 		$this->setName('cloudmarketwatch:awsupdate')
@@ -32,49 +33,27 @@ class AwsUpdateCommand extends ContainerAwareCommand {
 		$ec2Client = Ec2Client::factory($config);
 
 		// Get the max date
-		$maxDateQuery = $em
-				->createQuery(
-						"SELECT rh FROM CloudMarketWatch\BackendBundle\Entity\RunHistory rh ORDER BY rh.date, rh.id DESC");
-		$maxDateQuery->setMaxResults(1);
-		$maxDateQueryResult = $maxDateQuery->getOneOrNullResult();
-
-		// If we don't have a max date in the database, default to a week ago
-		if (is_null($maxDateQueryResult)
-				|| is_null($maxDateQueryResult->getDate())) {
-			$maxDateQueryResult = new RunHistory();
-			$oneWeekBack = new \DateTime();
-			$oneWeekBack->sub(new \DateInterval("P7D"));
-			$maxDateQueryResult->setDate($oneWeekBack);
-		}
+		$startDate = $this->getLastRunDate($em);
 
 		// Prime our request by setting the next token to an empty string
 		$nextToken = "";
 		$endDate = new \DateTime();
+
+		$output
+				->writeln(
+						"Start date: " . $startDate->format(\DateTime::ISO8601));
+		$output->writeln("End date: " . $endDate->format(\DateTime::ISO8601));
+
 		do {
 			// Make a request
+			$output->writeln('Querying AWS...');
 			$awsResponse = $ec2Client
 					->describeSpotPriceHistory(
-							array(
-									"StartTime" => $maxDateQueryResult
-											->getDate(), "EndTime" => $endDate,
+							array("StartTime" => $startDate,
+									"EndTime" => $endDate,
 									"NextToken" => $nextToken));
 
-			// Loop through results
-			// TODO Put this in some other function
-			foreach ($awsResponse['SpotPriceHistory'] as $history) {
-				
-				// TODO Find a way to handle the product type
-				$priceHistory = new PriceHistory();
-				$priceHistory
-						->setDate(
-								\DateTime::createFromFormat("Y-m-d*H:i:s|+",
-										$history['Timestamp']));
-				$priceHistory->setPrice($history['SpotPrice'] * 100 * 1000);
-				$priceHistory
-						->setAvailabilityZone($history['AvailabilityZone']);
-
-				$em->persist($priceHistory);
-			}
+			$this->persistPrices($awsResponse['SpotPriceHistory'], $em);
 
 			// Are there more?  Then loop again
 			$nextToken = $awsResponse['NextToken'];
@@ -82,12 +61,56 @@ class AwsUpdateCommand extends ContainerAwareCommand {
 		} while (!empty($nextToken));
 
 		// Record the fact that we did this operation
-		$runHistory = new RunHistory();
-		$runHistory->setDate($endDate);
-		$em->persist($runHistory);
+		$output->writeln('Finished querying AWS.  Writing histories.');
+		$this->persistRunHistory($endDate, $em);
 
 		// Flush to db
 		$em->flush();
+	}
+
+	// TODO Document me
+	private function persistPrices($histories, $em) {
+		foreach ($histories as $history) {
+
+			// TODO Find a way to handle the product type
+			$priceHistory = new PriceHistory();
+			$priceHistory
+					->setDate(
+							\DateTime::createFromFormat("Y-m-d*H:i:s|+",
+									$history['Timestamp']));
+			$priceHistory->setPrice($history['SpotPrice'] * 100 * 1000);
+			$priceHistory->setAvailabilityZone($history['AvailabilityZone']);
+
+			$em->persist($priceHistory);
+		}
+	}
+
+	// TODO Document me
+	private function persistRunHistory($endDate, $em) {
+		$runHistory = new RunHistory();
+		$runHistory->setDate($endDate);
+		$em->persist($runHistory);
+	}
+
+	// TODO Document me
+	private function getLastRunDate($em) {
+		$maxDateQuery = $em
+				->createQuery(
+						"SELECT rh FROM CloudMarketWatch\BackendBundle\Entity\RunHistory rh ORDER BY rh.date, rh.id DESC");
+		$maxDateQuery->setMaxResults(1);
+		$maxDateQueryResult = $maxDateQuery->getOneOrNullResult();
+		$lastRunDate = null;
+
+		// If we don't have a max date in the database, default to a week ago
+		if (is_null($maxDateQueryResult)
+				|| is_null($maxDateQueryResult->getDate())) {
+			$lastRunDate = new \DateTime();
+			$lastRunDate->sub(new \DateInterval("P7D"));
+		} else {
+			$lastRunDate = $maxDateQueryResult->getDate();
+		}
+
+		return $lastRunDate;
 	}
 }
 ?>
