cfc420fc04ddc8f3bda469f99a2a9a12320a9c46
Brian Stempin <brian.stempin@gmail.com>
2013-12-18 22:19:39 -0500

    Added logging and instance types

--- BackendBundle/Command/AwsUpdateCommand.php
+++ BackendBundle/Command/AwsUpdateCommand.php
@@ -2,8 +2,10 @@
 namespace CloudMarketWatch\BackendBundle\Command;
 
 use Aws\Ec2\Ec2Client;
+use Aws\Common\Enum\Region;
 
 use CloudMarketWatch\BackendBundle\Entity\PriceHistory;
+use CloudMarketWatch\BackendBundle\Entity\Product;
 use CloudMarketWatch\BackendBundle\Entity\RunHistory;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -14,65 +16,122 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 // TODO Add proper logging
 class AwsUpdateCommand extends ContainerAwareCommand {
+
+	/**
+	 * Entity manager for db access
+	 * @var unknown
+	 */
+	private $em;
+
+	/**
+	 * Logger for logging
+	 * @var unknown
+	 */
+	private $logger;
+
+	/**
+	 * Map of product types, keyed by instance type and distribution type
+	 * @var unknown
+	 */
+	private $productTypes;
+
+	/**
+	 * Static variable to let us know which platform we're working with
+	 * @var unknown
+	 */
+	private static $platform = "aws";
+
+	/**
+	 * Array of regions that we can't query, such as gov't regions
+	 * @var array of black-listed regions
+	 */
+	private static $regionBlacklist = array('us-gov-west-1');
+
 	protected function configure() {
 		$this->setName('cloudmarketwatch:awsupdate')
 				->setDescription('Grabs the latest market data from AWS');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		$em = $this->getContainer()->get('doctrine')->getManager();
+
+		$this->em = $this->getContainer()->get('doctrine')->getManager();
+		$this->logger = $this->getContainer()->get('logger');
+
+		$this->logger->info("AWS spot price update started");
 
 		// Create an array of configuration options for AWS
 		// TODO Don't hard-code the region in the future
 		$config = array(
 				'key' => $this->getContainer()->getParameter('aws_key'),
 				'secret' => $this->getContainer()->getParameter('aws_secret'),
-				'region' => 'us-east-1');
+				'region' => '');
 
+		// Amazon regions
+		// Filters out dups and the blacklist
+		$regions = array_diff(array_unique(Region::values()),
+				AwsUpdateCommand::$regionBlacklist);
+
+		$this->productTypes = $this->getProductTypes();
+
+		// Get the date bounds for our query
+		$startDate = $this->getLastRunDate();
+		$endDate = new \DateTime();
+
+		foreach ($regions as $currentRegion) {
+			$config['region'] = $currentRegion;
+			$this->getPricesUsingOptions($config, $startDate, $endDate);
+		}
+
+		// Record the fact that we did this operation
+		$this->persistRunHistory($endDate);
+
+		// Flush to db
+		$this->logger->info("Flushing data to db");
+		$this->em->flush();
+
+		$this->logger->info("Finished");
+	}
+
+	private function getPricesUsingOptions($awsConfig, \DateTime $startDate,
+			\DateTime $endDate) {
 		// Amazon client
-		$ec2Client = Ec2Client::factory($config);
-
-		// Get the max date
-		$startDate = $this->getLastRunDate($em);
+		$ec2Client = Ec2Client::factory($awsConfig);
 
 		// Prime our request by setting the next token to an empty string
 		$nextToken = "";
-		$endDate = new \DateTime();
 
-		$output
-				->writeln(
-						"Start date: " . $startDate->format(\DateTime::ISO8601));
-		$output->writeln("End date: " . $endDate->format(\DateTime::ISO8601));
+		$this->logger
+				->info(
+						"Getting history for region " . $awsConfig['region']
+								. " between "
+								. $startDate->format(\DateTime::ISO8601)
+								. " and "
+								. $endDate->format(\DateTime::ISO8601));
 
 		do {
 			// Make a request
-			$output->writeln('Querying AWS...');
 			$awsResponse = $ec2Client
 					->describeSpotPriceHistory(
 							array("StartTime" => $startDate,
 									"EndTime" => $endDate,
 									"NextToken" => $nextToken));
 
-			$this->persistPrices($awsResponse['SpotPriceHistory'], $em);
+			$this->persistPrices($awsResponse['SpotPriceHistory']);
 
 			// Are there more?  Then loop again
 			$nextToken = $awsResponse['NextToken'];
 
 		} while (!empty($nextToken));
 
-		// Record the fact that we did this operation
-		$output->writeln('Finished querying AWS.  Writing histories.');
-		$this->persistRunHistory($endDate, $em);
-
-		// Flush to db
-		$em->flush();
+		$this->logger->info("Done");
 	}
 
 	// TODO Document me
-	private function persistPrices($histories, $em) {
+	private function persistPrices($histories) {
 		foreach ($histories as $history) {
 
-			// TODO Find a way to handle the product type
+			$product = $this->getProduct($history);
+
 			$priceHistory = new PriceHistory();
 			$priceHistory
 					->setDate(
@@ -80,21 +139,52 @@ class AwsUpdateCommand extends ContainerAwareCommand {
 									$history['Timestamp']));
 			$priceHistory->setPrice($history['SpotPrice'] * 100 * 1000);
 			$priceHistory->setAvailabilityZone($history['AvailabilityZone']);
+			$priceHistory->setProduct($product);
 
-			$em->persist($priceHistory);
+			$this->em->persist($priceHistory);
 		}
 	}
 
 	// TODO Document me
-	private function persistRunHistory($endDate, $em) {
-		$runHistory = new RunHistory();
-		$runHistory->setDate($endDate);
-		$em->persist($runHistory);
+	private function getProduct($history) {
+
+		$instanceType = $history['InstanceType'];
+		$distributionType = $history['ProductDescription'];
+
+		if (array_key_exists($instanceType, $this->productTypes)
+				&& array_key_exists($distributionType,
+						$this->productTypes[$instanceType])) {
+			return $this->productTypes[$instanceType][$distributionType];
+		} else {
+			$this->logger
+					->info(
+							"Found a new product type: " . $instanceType
+									. " -- " . $distributionType);
+
+			$product = new Product();
+			$product->setDistributionType($distributionType);
+			$product->setInstanceType($instanceType);
+			$product->setPlatform(AwsUpdateCommand::$platform);
+
+			$this->productTypes[$instanceType][$distributionType] = $product;
+			$this->em->persist($product);
+
+			return $product;
+		}
 	}
 
 	// TODO Document me
-	private function getLastRunDate($em) {
-		$maxDateQuery = $em
+	private function persistRunHistory($endDate) {
+		$runHistory = new RunHistory();
+		$runHistory->setDate($endDate);
+		$this->em->persist($runHistory);
+	}
+
+	// TODO Document me
+	private function getLastRunDate() {
+		$this->logger->info("Finding the last run date");
+
+		$maxDateQuery = $this->em
 				->createQuery(
 						"SELECT rh FROM CloudMarketWatch\BackendBundle\Entity\RunHistory rh ORDER BY rh.date, rh.id DESC");
 		$maxDateQuery->setMaxResults(1);
@@ -111,6 +201,23 @@ class AwsUpdateCommand extends ContainerAwareCommand {
 		}
 
 		return $lastRunDate;
+	}
+
+	// TODO Document me
+	private function getProductTypes() {
+		$this->logger->info("Producing product map");
+
+		$products = $this->em
+				->getRepository('CloudMarketWatch\BackendBundle\Entity\Product')
+				->findByPlatform(AwsUpdateCommand::$platform);
+		$productMap = array();
+
+		foreach ($products as $product) {
+			$productMap[$product->getInstanceType()][$product
+					->getDistributionType()] = $product;
+		}
+
+		return $productMap;
 	}
 }
 ?>
