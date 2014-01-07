243843ecd9a5513d3b7efe8f5fbb53dd8e0a842f
Brian Stempin <brian.stempin@gmail.com>
2014-01-07 01:37:17 -0500

    Documentation update

--- BackendBundle/Command/AwsUpdateCommand.php
+++ BackendBundle/Command/AwsUpdateCommand.php
@@ -8,13 +8,21 @@ use CloudMarketWatch\BackendBundle\Entity\PriceHistory;
 use CloudMarketWatch\BackendBundle\Entity\Product;
 use CloudMarketWatch\BackendBundle\Entity\RunHistory;
 
+use Monolog\Logger;
+
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-// TODO Add proper logging
+/**
+ * Command that handles scraping for new AWS market prices
+ * 
+ * This command takes no arguments since all that it needs comes from the container.  It will use the database to figure
+ * out the last time that it ran, perform a query against AWS, record the results, and update the database to reflect
+ * that it ran.
+ */
 class AwsUpdateCommand extends ContainerAwareCommand {
 
 	/**
@@ -25,44 +33,55 @@ class AwsUpdateCommand extends ContainerAwareCommand {
 
 	/**
 	 * Handle to the lock file
+	 * @var resource
 	 */
 	private $lockFile;
-	
+
 	/**
 	 * Name of the lock file
-	 * @var unknown
+	 * @var string
 	 */
 	private static $lockFileName = "awsupdate.lock";
-	
+
 	/**
 	 * Logger for logging
-	 * @var unknown
+	 * @var Logger
 	 */
 	private $logger;
 
 	/**
 	 * Map of product types, keyed by instance type and distribution type
-	 * @var unknown
+	 * @var array of ProductType
 	 */
 	private $productTypes;
 
 	/**
 	 * Static variable to let us know which platform we're working with
-	 * @var unknown
+	 * @var string
 	 */
 	private static $platform = "aws";
 
+	// TODO Pull this from the container instead
 	/**
 	 * Array of regions that we can't query, such as gov't regions
 	 * @var array of black-listed regions
 	 */
 	private static $regionBlacklist = array('us-gov-west-1');
 
+	/**
+	 * Registers command with Symfony
+	 */
 	protected function configure() {
 		$this->setName('cloudmarketwatch:awsupdate')
 				->setDescription('Grabs the latest market data from AWS');
 	}
 
+	/**
+	 * Main entry point called by Symfony
+	 * 
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 */
 	protected function execute(InputInterface $input, OutputInterface $output) {
 
 		$this->em = $this->getContainer()->get('doctrine')->getManager();
@@ -111,8 +130,18 @@ class AwsUpdateCommand extends ContainerAwareCommand {
 		$this->logger->info("Finished");
 	}
 
-	private function getPricesUsingOptions($awsConfig, \DateTime $startDate,
-			\DateTime $endDate) {
+	/**
+	 * Uses the supplied configuration to call the EC2 service and query for prices between the specified dates
+	 * 
+	 * This method handles calling the service multiple times should the result paginate.  As per Amazon spec, the
+	 * prices returned might be outside of the date range.
+	 * 
+	 * @param array $awsConfig Ec2Client config
+	 * @param \DateTime $startDate Date to start search, inclusive
+	 * @param \DateTime $endDate Date to end search, inclusive
+	 */
+	private function getPricesUsingOptions(array $awsConfig,
+			\DateTime $startDate, \DateTime $endDate) {
 		// Amazon client
 		$ec2Client = Ec2Client::factory($awsConfig);
 
@@ -152,19 +181,34 @@ class AwsUpdateCommand extends ContainerAwareCommand {
 		$this->logger->info("Done");
 	}
 
-	// TODO Document me
+	/**
+	 * Detects if another AWS update is running
+	 * 
+	 * This method uses a file lock to detect if an update is already running.  This implementation is cross-platform.
+	 * 
+	 * @return boolean true if a dup process is running, false otherwise
+	 */
 	private function isDuplicateProcessRunning() {
 		$this->lockFile = fopen("awsupdate.lock", "w");
-		
+
 		return !flock($this->lockFile, LOCK_NB | LOCK_EX);
 	}
 
-	// TODO Document me
-	private function persistPrices($histories, \DateTime $startDate,
+	/**
+	 * Persists the price histories
+	 * 
+	 * This method iterates through an array of PriceHistory, checks that the date of each one is acceptable, and
+	 * persists.
+	 * 
+	 * @param array $histories array of PriceHistory
+	 * @param \DateTime $startDate Start date to filter with, exclusive
+	 * @param \DateTime $endDate End date to filter with, inclusive
+	 */
+	private function persistPrices(array $histories, \DateTime $startDate,
 			\DateTime $endDate) {
 		foreach ($histories as $history) {
 
-			$product = $this->getProduct($history);
+			$product = $this->getProductForHistory($history);
 
 			$priceHistory = new PriceHistory();
 			$priceHistory
@@ -186,8 +230,16 @@ class AwsUpdateCommand extends ContainerAwareCommand {
 		}
 	}
 
-	// TODO Document me
-	private function getProduct($history) {
+	/**
+	 * Gest the Product for the current PriceHistory
+	 * 
+	 * This method will look at the PriceHistory and attempt to find a Product object that matches.  If no such object
+	 * exists, then one will be created.
+	 * 
+	 * @param array $history A history array from the EC2 response
+	 * @return \CloudMarketWatch\BackendBundle\Entity\Product An object representing the product that the price is for
+	 */
+	private function getProductForHistory(array $history) {
 
 		$instanceType = $history['InstanceType'];
 		$distributionType = $history['ProductDescription'];
@@ -214,14 +266,22 @@ class AwsUpdateCommand extends ContainerAwareCommand {
 		}
 	}
 
-	// TODO Document me
-	private function persistRunHistory($endDate) {
+	/**
+	 * Makes an entry in the database to show that a run completed
+	 * 
+	 * @param DateTime $endDate Date to record
+	 */
+	private function persistRunHistory(\DateTime $endDate) {
 		$runHistory = new RunHistory();
 		$runHistory->setDate($endDate);
 		$this->em->persist($runHistory);
 	}
 
-	// TODO Document me
+	/**
+	 * Gets the last end date from teh database
+	 * 
+	 * @return Ambigous <NULL, \DateTime, multitype:> null if it none exists, DateTime otherwise
+	 */
 	private function getLastRunDate() {
 		$this->logger->info("Finding the last run date");
 
@@ -244,7 +304,14 @@ class AwsUpdateCommand extends ContainerAwareCommand {
 		return $lastRunDate;
 	}
 
-	// TODO Document me
+	/**
+	 * Gets an array of known Products from the database
+	 * 
+	 * Because the list of products is small, we don't want to hit the database over and over querying for product
+	 * types.  Rather, we want to query for them all and to store them in memory for quick mapping.
+	 * 
+	 * @return Ambigous <multitype:, array> null if no entries exists, array of Product otherwise
+	 */
 	private function getProductTypes() {
 		$this->logger->info("Producing product map");
 
